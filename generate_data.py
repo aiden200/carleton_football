658f89f74fa00c1c818fa1fbbae8da4f7d7b52af
Aiden Chang <aidenchang@MacBook-Pro-2.local>
2022-01-28 20:47:32 -0600

    create_new_recruits functions halfway

--- generate_data.py
+++ generate_data.py
@@ -2,7 +2,7 @@ import sys
 import csv
 import glob
 
-
+DEBUG = False
 '''
 Merges all the different csv files in the subdirectory ncsa_data. 
 Must be encoded in utf-8 format.
@@ -13,9 +13,6 @@ Columns are
     'Weight', 'High School', 'Club Team', 'Phone', 'Email', 'Address', 'City', 'State', 'Zip', 
     'Rating', 'GPA', 'Class Rank'
 '''
-
-
-
 def merge_NCSA_Data():
     with open("./final_data_sets/ncsa_combined_data.csv", 'w', encoding = "UTF-8", newline = '') as wFile:
         writer = csv.writer(wFile)
@@ -69,24 +66,60 @@ def generate_name(name):
     return name
 
 
+'''
+Creates new recruits from NCSA data that are not in frontrush.
+Reads from "./final_data_sets/ncsa_combined_data.csv"
+'''
+def create_new_recruits(front_rush_data):
+    with open("./final_data_sets/new_recruits.csv", 'w', encoding = "UTF-8", newline = '') as wFile:
+        writer = csv.writer(wFile)
+        writer.writerow(['Last Name', 'First', 'HS', 'Email', 'Twitter', 'Cell Phone', \
+            'Address', 'City', 'State', 'Zip', 'Video Link', 'Send to Admissions', 'Source', \
+                'Recruiting Coach', 'Grad Year', 'Primary Position', 'Positions (All)', 'GPA', 'ACT', 'SAT'])
+                
+
+
 def testFunction():
-    path = "./ncsa_data/*.csv"
-    for fileName in glob.glob(path):
-        print(fileName) 
+    path = "./formatting_examples/example_format.csv"
+    '''for fileName in glob.glob(path):
+        print(fileName) '''
+    
+    print(['Last Name', 'First', 'HS', 'Email', 'Twitter', 'Cell Phone', \
+            'Address', 'City', 'State', 'Zip', 'Video Link', 'Send to Admissions', 'Source', \
+                'Recruiting Coach', 'Grad Year', 'Primary Position', 'Positions (All)', 'GPA', 'ACT', 'SAT'])
+    '''
+    f = open(path, 'r', encoding = "UTF-8")
+    with f as rFile:
+        spamreader = csv.reader(rFile, delimiter=',')
+        #next(spamreader)
+        for line in spamreader:
+            print(line)
+            break
+    '''
 
 '''
 main methode that calls all the functions. Will generate one big dataset, then load all the data from
 front rush recruits. Then will cross reference and write
 '''
 def main():
-    merge_NCSA_Data()
-    front_rush_data = gather_front_rush_data()
-    #create a dic that maps the columns from NCSA to front rush
-    #create a function that cross references the combined_ncsa_data and front rush dic to either add or write new lines into it
-        #We can seperate this into three fields, one for new recruits
-        #one with the old recruits but updated info
-        #one combined with both of them
-        #make sure to label the correct coaches and watch for null spaces. Add NCSA for source, good luck!
-    #testFunction()
+    if DEBUG:
+        testFunction()
+    else:
+        print("Starting Program...")
+        print('Merging NCSA datasheets...')
+        merge_NCSA_Data()
+        print("Done\nCreated merge file in ncsa_combined_data.csv in directory final_data_sets\n\
+            Loading front rush recruit data...")
+        front_rush_data = gather_front_rush_data()
+        print("Done\n cross referencing... this might take some time")
+        create_new_recruits(front_rush_data)
+        print("Created new Recruit data in new_recruits.csv")
+        #create a dic that maps the columns from NCSA to front rush
+        #create a function that cross references the combined_ncsa_data and front rush dic to either add or write new lines into it
+            #We can seperate this into three fields, one for new recruits
+            #one with the old recruits but updated info
+            #one combined with both of them
+            #make sure to label the correct coaches and watch for null spaces. Add NCSA for source, good luck!
+
 
 main()
